9f65117d1565b967a70e29b0a593600534eac76d
Md Tamanne Alam <127785840+tamanne2@users.noreply.github.com>
2024-08-31 07:23:25 +0000

    ad function

---
+++ .devcontainer/practice/function.js
@@ -0,0 +1,15 @@
+function anyobject(adobj){
+    console.log(`name is ${adobj.name} price is ${adobj.price}`)
+}
+
+let object = anyobject({
+    name: "Alam",
+    price: 35664
+})
+
+function arrr(arra){
+return arra[1]
+}
+let ar = (arrr([45,5,54,]))
+// console.log(arrr([45,5,54,]))
+console.log(ar)
